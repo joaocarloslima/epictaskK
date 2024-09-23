a0f3a48a99cd78b142b719c01d05997902a169b1
joaocarloslima <joaocarloslima@me.com>
2024-09-23 13:05:08 -0300

    lista de tarefas

---
+++ src/main/java/br/com/fiap/epictaskk/EpictaskKApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.epictaskk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpictaskKApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpictaskKApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/epictaskk/EpictaskKApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.epictaskk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpictaskKApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
