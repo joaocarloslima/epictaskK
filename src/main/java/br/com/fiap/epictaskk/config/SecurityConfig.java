2a413a3e088c8085953d0ae67cfde5e3e160c724
joaocarloslima <joaocarloslima@me.com>
2024-10-07 12:02:16 -0300

    oauth2

---
+++ src/main/java/br/com/fiap/epictaskk/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package br.com.fiap.epictaskk.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2Login(login -> login.loginPage("/login").permitAll())
+                .logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/login"))
+                //.formLogin(Customizer.withDefaults())
+        ;
+
+        return http.build();
+    }
+
+}
